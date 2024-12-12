4829bc3c70588b34e90d8430ff7e7ab072cf5b0f
Yasser Ben Moh <ybenmoh@enseirb-matmeca.fr>
2024-12-12 11:45:54 +0100

    first

---
+++ src/main/java/sejour/Trajet.java
@@ -0,0 +1,14 @@
+package main.java.sejour;
+
+import java.time.Instant;
+
+public class Trajet {
+
+    private String villeDepart;
+    private String villeArrivee;
+    private Instant tempsDepart;
+    private Instant tempsArrivee;
+
+
+
+}
